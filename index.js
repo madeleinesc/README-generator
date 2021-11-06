4c1d030084b58c531d27d2609f9fef89cf9b8d8a
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-11-06 13:04:40 +1030

    added some questions to array

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a short description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide instructions on how to install your project.',
+    },
+    
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
